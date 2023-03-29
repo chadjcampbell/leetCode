e97e55b3ce8ef2b4a8ace499d0ed1731721f6639
Chad <chadjcampbell@gmail.com>
2023-03-29 14:32:43 -0400

    design-linked-list

--- 707.design-linked-list.ts
+++ 707.design-linked-list.ts
@@ -8,27 +8,100 @@
 
 class LLNode {
   val: number;
-  next: Node | null;
-  constructor(val: number, next: Node | null) {
-    val = this.val;
-    next = this.next;
+  next: LLNode | null;
+  constructor(val?: number, next?: LLNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
   }
 }
 
 class MyLinkedList {
-  constructor() {}
+  head: LLNode | null;
+  constructor(head = null) {
+    this.head = head;
+  }
 
-  get(index: number): number {}
+  get(index: number): number {
+    if (this.head === null) return -1;
+    if (index === 0) return this.head.val;
+    let temp = this.head.next;
+    let count = 0;
+    while (count !== index) {
+      count++;
+      if (temp === null) return -1;
+      if (count === index) {
+        return temp.val;
+      }
+      temp = temp.next;
+    }
+    return -1;
+  }
 
-  addAtHead(val: number): void {}
+  addAtHead(val: number): void {
+    if (this.head === null) {
+      this.head = new LLNode(val);
+      return;
+    }
+    let newHead = new LLNode(val);
+    let temp = this.head;
+    newHead.next = temp;
+    this.head = newHead;
+  }
 
-  addAtTail(val: number): void {}
+  addAtTail(val: number): void {
+    if (this.head === null) {
+      this.head = new LLNode(val);
+      return;
+    }
+    let temp = this.head;
+    while (temp.next !== null) {
+      temp = temp.next;
+    }
+    temp.next = new LLNode(val);
+  }
 
-  addAtIndex(index: number, val: number): void {}
+  addAtIndex(index: number, val: number): void {
+    if (index === 0) this.addAtHead(val);
+    let temp = this.head;
+    let count = 0;
+    while (count !== index) {
+      count++;
+      if (temp === null) return;
+      if (count === index) {
+        let newNode = new LLNode(val);
+        newNode.next = temp.next;
+        temp.next = newNode;
+      }
+      temp = temp.next;
+    }
+    return;
+  }
 
-  deleteAtIndex(index: number): void {}
+  deleteAtIndex(index: number): void {
+    if (this.head === null) return;
+    let temp = this.head;
+    let count = 0;
+    if (index === 0) {
+      temp = this.head.next;
+      this.head = temp;
+      return;
+    }
+    while (count !== index) {
+      count++;
+      if (temp === null) return;
+      if (count === index) {
+        if (temp.next === null) {
+          temp = null;
+          return;
+        }
+        temp.next = temp.next.next;
+        return;
+      }
+      temp = temp.next;
+    }
+    return;
+  }
 }
-
 /**
  * Your MyLinkedList object will be instantiated and called as such:
  * var obj = new MyLinkedList()
