2afdb2ec2ace7f197045e6369c927135ddf2b68f
ChadsLaptop <chadjcampbell@gmail.com>
2023-04-02 18:24:32 -0400

    sort-an-array (merge sort)

--- 912.sort-an-array.ts
+++ 912.sort-an-array.ts
@@ -5,7 +5,40 @@
  */
 
 // @lc code=start
+
+function merge(left: number[], right: number[]) {
+  let i = 0,
+    j = 0;
+  let sorted: number[] = [];
+  while (left[i] !== undefined && right[j] !== undefined) {
+    if (left[i] <= right[j]) {
+      sorted.push(left[i]);
+      i++;
+    } else {
+      sorted.push(right[j]);
+      j++;
+    }
+  }
+  if (left[i] !== undefined) {
+    sorted.push(...left.slice(i));
+  }
+  if (right[j] !== undefined) {
+    sorted.push(...right.slice(j));
+  }
+  return sorted;
+}
+
 function sortArray(nums: number[]): number[] {
+  if (nums.length <= 1) return nums;
+  let mid = Math.floor(nums.length / 2);
+  let left = sortArray(nums.slice(0, mid));
+  let right = sortArray(nums.slice(mid));
+  let sorted = merge(left, right);
+  return sorted;
+}
+
+//insertion sort
+/* function sortArray(nums: number[]): number[] {
   for (let i = 1; i < nums.length; i++) {
     let j = i - 1;
     while (j >= 0 && nums[j] > nums[j + 1]) {
@@ -16,5 +49,5 @@ function sortArray(nums: number[]): number[] {
     }
   }
   return nums;
-}
+} */
 // @lc code=end
