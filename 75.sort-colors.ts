c9b9b2131841b33562ae5279779796a64a37a361
ChadsLaptop <chadjcampbell@gmail.com>
2023-04-05 14:56:45 -0400

    sort-colors (with quickSort)

---
+++ 75.sort-colors.ts
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=75 lang=typescript
+ *
+ * [75] Sort Colors
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+
+//in place quick sort
+function sortColors(nums: number[]): void {
+  function quickSort(arr, s, e) {
+    if (e - s + 1 <= 1) {
+      return arr;
+    }
+
+    let pivot = arr[e];
+    let left = s; // pointer for left side
+
+    // Partition: elements smaller than pivot on left side
+    for (let i = s; i < e; i++) {
+      if (arr[i] < pivot) {
+        let tmp = arr[left];
+        arr[left] = arr[i];
+        arr[i] = tmp;
+        left++;
+      }
+    }
+
+    // Move pivot in-between left & right sides
+    arr[e] = arr[left];
+    arr[left] = pivot;
+
+    // Quick sort left side
+    quickSort(arr, s, left - 1);
+
+    // Quick sort right side
+    quickSort(arr, left + 1, e);
+
+    return arr;
+  }
+  return quickSort(nums, 0, nums.length - 1);
+}
+// @lc code=end
