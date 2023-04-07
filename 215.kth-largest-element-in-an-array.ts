2090c5f6a18b16baa71a6c70d5c519d6c3eab408
Chad <chadjcampbell@gmail.com>
2023-04-07 18:43:06 -0400

    kth-largest-element (with modified quickSort)

--- 215.kth-largest-element-in-an-array.ts
+++ 215.kth-largest-element-in-an-array.ts
@@ -6,7 +6,29 @@
 
 // @lc code=start
 function findKthLargest(nums: number[], k: number): number {
-  nums.sort((a, b) => a - b);
-  return nums[nums.length - k];
+  k = nums.length - k;
+  function quickSortToK(start: number, end: number): number {
+    if (end - start <= 0) return nums[k];
+    let pivot = nums[end];
+    let left = start;
+    for (let i = start; i < end; i++) {
+      if (nums[i] < pivot) {
+        let temp = nums[i];
+        nums[i] = nums[left];
+        nums[left] = temp;
+        left++;
+      }
+    }
+    nums[end] = nums[left];
+    nums[left] = pivot;
+    if (k < left) {
+      quickSortToK(start, left - 1);
+    }
+    if (k > left) {
+      quickSortToK(left + 1, end);
+    }
+    return nums[k];
+  }
+  return quickSortToK(0, nums.length - 1);
 }
 // @lc code=end
