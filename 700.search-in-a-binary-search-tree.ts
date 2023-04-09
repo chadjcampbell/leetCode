fe0d375e7cdcf1272ffa6ff7d4ccffbd34be342f
ChadsLaptop <chadjcampbell@gmail.com>
2023-04-09 14:48:19 -0400

    search-in-a-binary-search-tree

---
+++ 700.search-in-a-binary-search-tree.ts
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=700 lang=typescript
+ *
+ * [700] Search in a Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function searchBST(root: TreeNode | null, val: number): TreeNode | null {
+  if (root === null) return null;
+  if (root.val === val) {
+    return root;
+  }
+  if (root.val > val) {
+    return searchBST(root.left, val);
+  }
+  if (root.val < val) {
+    return searchBST(root.right, val);
+  }
+  return null;
+}
+// @lc code=end
