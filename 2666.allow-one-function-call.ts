65c7d8c6b55f684a62be6c0b01501b6b603096ee
chadjcampbell <chadjcampbell@gmail.com>
2023-05-12 13:02:32 -0400

    allow-one-function-call (done)

--- 2666.allow-one-function-call.ts
+++ 2666.allow-one-function-call.ts
@@ -1,7 +1,16 @@
 function once<T extends (...args: any[]) => any>(
   fn: T
 ): (...args: Parameters<T>) => ReturnType<T> | undefined {
-  return function (...args) {};
+  let hasBeenCalled = false;
+
+  return function (...args) {
+    if (hasBeenCalled) {
+      return undefined;
+    } else {
+      hasBeenCalled = true;
+      return fn(...args);
+    }
+  };
 }
 
 /**
