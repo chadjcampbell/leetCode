99fcfbaa2aa76dfc0cb9b518017511dec9681e5c
Chad <chadjcampbell@gmail.com>
2023-05-14 11:42:50 -0400

    curry (done)

--- 2632.curry.ts
+++ 2632.curry.ts
@@ -1,7 +1,12 @@
 function curry(fn: Function): Function {
-  return function curried() {};
+  return function curried(...args: any[]) {
+    if (args.length >= fn.length) {
+      return fn(...args);
+    } else {
+      return curried.bind(this, ...args);
+    }
+  };
 }
-
 /**
  * function sum(a, b) { return a + b; }
  * const csum = curry(sum);
