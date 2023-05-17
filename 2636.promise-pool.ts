590495ad6f48bc838a92418bff49de15478ed7d9
ChadsLaptop <chadjcampbell@gmail.com>
2023-05-17 14:52:56 -0400

    promise-pool (done)

--- 2636.promise-pool.ts
+++ 2636.promise-pool.ts
@@ -1,6 +1,16 @@
 type F = () => Promise<any>;
 
-function promisePool(functions: F[], n: number): Promise<any> {}
+async function promisePool(functions: F[], n: number): Promise<any> {
+  async function promiseQueue() {
+    while (functions.length !== 0) {
+      const firstFunction = functions.shift();
+      await firstFunction!();
+      await promiseQueue();
+    }
+  }
+  const promiseArray = Array(n).fill(0).map(promiseQueue);
+  await Promise.all(promiseArray);
+}
 
 /**
  * const sleep = (t) => new Promise(res => setTimeout(res, t));
