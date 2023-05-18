e111db925f5892b90670c209f89114754b4db70f
chadjcampbell <chadjcampbell@gmail.com>
2023-05-18 12:35:50 -0400

    cache-with-time-limit (done)

--- 2622.cache-with-time-limit.ts
+++ 2622.cache-with-time-limit.ts
@@ -1,11 +1,41 @@
 class TimeLimitedCache {
-  constructor() {}
+  cache: Map<number, number> = new Map();
+  timers: Map<number, any> = new Map();
+  constructor() {
+    this.cache = new Map();
+    this.timers = new Map();
+  }
 
-  set(key: number, value: number, duration: number): boolean {}
+  set(key: number, value: number, duration: number): boolean {
+    if (this.cache.has(key)) {
+      clearInterval(this.timers.get(key));
+      this.cache.set(key, value);
+      const timerId = setTimeout(() => {
+        this.cache.delete(key);
+      }, duration);
+      this.timers.set(key, timerId);
+      return true;
+    } else {
+      this.cache.set(key, value);
+      const timerId = setTimeout(() => {
+        this.cache.delete(key);
+      }, duration);
+      this.timers.set(key, timerId);
+      return false;
+    }
+  }
 
-  get(key: number): number {}
+  get(key: number): number {
+    if (this.cache.has(key)) {
+      return this.cache.get(key)!;
+    } else {
+      return -1;
+    }
+  }
 
-  count(): number {}
+  count(): number {
+    return this.cache.size;
+  }
 }
 
 /**
