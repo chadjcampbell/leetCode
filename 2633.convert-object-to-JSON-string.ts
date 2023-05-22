36c6b149120bdad410c5550334ca774c71f3382c
chadjcampbell <chadjcampbell@gmail.com>
2023-05-22 17:23:22 -0400

    convert-object-to-JSON-string (done)

--- 2633.convert-object-to-JSON-string.ts
+++ 2633.convert-object-to-JSON-string.ts
@@ -1,1 +1,26 @@
-function jsonStringify(object: any): string {}
+function jsonStringify(object: any): string {
+  let returnString = "";
+  if (typeof object === "string") return `"${object}"`;
+  if (object === null) return `null`;
+  if (typeof object !== "object") return `${String(object)}`;
+  if (Array.isArray(object)) {
+    returnString += `[`;
+    for (let i = 0; i < object.length; i++) {
+      returnString += `${jsonStringify(object[i])}`;
+      if (i < object.length - 1) returnString += `,`;
+    }
+    returnString += `]`;
+  }
+  if (typeof object === "object" && !Array.isArray(object)) {
+    returnString += `{`;
+    const objEntries = Object.entries(object);
+    for (let i = 0; i < objEntries.length; i++) {
+      returnString += `${jsonStringify(objEntries[i][0])}:${jsonStringify(
+        objEntries[i][1]
+      )}`;
+      if (i < objEntries.length - 1) returnString += `,`;
+    }
+    returnString += `}`;
+  }
+  return returnString;
+}
