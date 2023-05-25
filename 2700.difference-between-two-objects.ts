fd10d49ab90bd343deeab607157f765fc1b79ecb
chadjcampbell <chadjcampbell@gmail.com>
2023-05-25 12:27:14 -0400

    difference-between-two-objects (done)

--- 2700.difference-between-two-objects.ts
+++ 2700.difference-between-two-objects.ts
@@ -1,14 +1,31 @@
 function objDiff(obj1: any, obj2: any): any {
+  if (!isObject(obj1) && !isObject(obj2)) {
+    //both primitive
+    return obj1 === obj2 ? {} : [obj1, obj2];
+  }
+  if (!isObject(obj1) || !isObject(obj2)) {
+    // one primitive
+    return [obj1, obj2];
+  }
+  if (Array.isArray(obj1) !== Array.isArray(obj2)) {
+    // one array, one object
+    return [obj1, obj2];
+  }
   let newObj = {};
-  //Only compare common keys
+  //both arr, or both obj, use recursion
+  for (const key in obj1) {
+    //only compare common keys
+    if (key in obj2) {
+      const diff = objDiff(obj1[key], obj2[key]);
+      if (Object.keys(diff).length > 0) {
+        newObj[key] = diff;
+      }
+    }
+  }
 
-  //If both primitave and diff, then diff
-
-  //If one is obj and other isn't, then diff
-
-  //If one is array and one is obj, then diff
-
-  //If both arr, or both obj, use recursion
+  function isObject(obj: any) {
+    return obj !== null && typeof obj === "object";
+  }
 
   return newObj;
 }
