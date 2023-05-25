3913acbea7d67252bd79e25924c3b6ed266a5cdd
chadjcampbell <chadjcampbell@gmail.com>
2023-05-25 12:40:55 -0400

    chunk-array (done)

--- 2677.chunk-array.ts
+++ 2677.chunk-array.ts
@@ -1,1 +1,14 @@
-function chunk(arr: any[], size: number): any[][] {}
+function chunk(arr: any[], size: number): any[][] {
+  let result: any[][] = [];
+  let rollingIndex = 0;
+  while (rollingIndex < arr.length) {
+    let currentChunk: any[] = [];
+    for (let i = 0; i < size; i++) {
+      if (arr[rollingIndex] !== undefined) {
+        currentChunk.push(arr[rollingIndex++]);
+      }
+    }
+    result.push(currentChunk);
+  }
+  return result;
+}
