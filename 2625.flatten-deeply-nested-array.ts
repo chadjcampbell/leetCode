83840c200063d1073390c1a6e97091a65e870c7e
chadjcampbell <chadjcampbell@gmail.com>
2023-05-26 12:40:50 -0400

    flatten-deeply-nested-array (done)

--- 2625.flatten-deeply-nested-array.ts
+++ 2625.flatten-deeply-nested-array.ts
@@ -3,4 +3,19 @@ type MultiDimensionalArray = (number | MultiDimensionalArray)[];
 var flat = function (
   arr: MultiDimensionalArray,
   n: number
-): MultiDimensionalArray {};
+): MultiDimensionalArray {
+  let result: MultiDimensionalArray = [];
+
+  function flatDepth(array: MultiDimensionalArray, depth: number) {
+    for (const value of array) {
+      if (Array.isArray(value) && depth < n) {
+        flatDepth(value, depth + 1);
+      } else {
+        result.push(value);
+      }
+    }
+    return result;
+  }
+
+  return flatDepth(arr, 0);
+};
