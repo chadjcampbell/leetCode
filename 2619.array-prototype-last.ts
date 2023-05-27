7b942ed991ce6e65551982e44246000c89404f1a
chadjcampbell <chadjcampbell@gmail.com>
2023-05-27 11:52:27 -0400

    array-prototype-last (setup)

---
+++ 2619.array-prototype-last.ts
@@ -0,0 +1,14 @@
+declare global {
+  interface Array<T> {
+    last(): T | -1;
+  }
+}
+
+Array.prototype.last = function () {};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
+
+export {};
