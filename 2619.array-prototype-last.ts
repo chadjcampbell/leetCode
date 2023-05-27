dca7f72d7b55ef311bbb6f3ec7ca3f877c21f7fb
chadjcampbell <chadjcampbell@gmail.com>
2023-05-27 11:56:08 -0400

    array-prototype-last (done)

--- 2619.array-prototype-last.ts
+++ 2619.array-prototype-last.ts
@@ -4,7 +4,13 @@ declare global {
   }
 }
 
-Array.prototype.last = function () {};
+Array.prototype.last = function () {
+  if (this.length <= 0) {
+    return -1;
+  } else {
+    return this[this.length - 1];
+  }
+};
 
 /**
  * const arr = [1, 2, 3];
