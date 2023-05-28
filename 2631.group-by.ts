1322abdad66a049258f054d7f46dc5714a571ad3
ChadsLaptop <chadjcampbell@gmail.com>
2023-05-28 08:13:44 -0400

    group-by (setup)

---
+++ 2631.group-by.ts
@@ -0,0 +1,11 @@
+declare global {
+  interface Array<T> {
+    groupBy(fn: (item: T) => string): Record<string, T[]>;
+  }
+}
+
+Array.prototype.groupBy = function (fn) {};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
