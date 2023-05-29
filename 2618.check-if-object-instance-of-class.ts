58983cf643e7ede443d40f5f1a1f98a9ffb2373e
ChadsLaptop <chadjcampbell@gmail.com>
2023-05-29 11:12:47 -0400

    check-if-object-instance-of-class (done)

--- 2618.check-if-object-instance-of-class.ts
+++ 2618.check-if-object-instance-of-class.ts
@@ -1,4 +1,22 @@
-function checkIfInstanceOf(obj: any, classFunction: any): boolean {}
+function checkIfInstanceOf(obj: any, classFunction: any): boolean {
+  if (
+    obj === null ||
+    obj === undefined ||
+    typeof classFunction !== "function"
+  ) {
+    return false;
+  }
+
+  let proto = Object.getPrototypeOf(obj);
+
+  while (proto !== null) {
+    if (proto === classFunction.prototype) {
+      return true;
+    }
+    proto = Object.getPrototypeOf(proto);
+  }
+  return false;
+}
 
 /**
  * checkIfInstanceOf(new Date(), Date); // true
