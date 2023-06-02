acd6791fdd911b8f5f5367cd5e32b408b18775fd
chadjcampbell <chadjcampbell@gmail.com>
2023-06-02 09:38:53 -0400

    generate-fibonacci-sequence (done)

--- 2648.generate-fibonacci-sequence.ts
+++ 2648.generate-fibonacci-sequence.ts
@@ -1,7 +1,24 @@
-function* fibGenerator(): Generator<number, any, number> {}
+function* fibGenerator(): Generator<number, any, number> {
+  let fib1: number = 0;
+  let fib2: number = 1;
+  yield fib1;
+  yield fib2;
+  yield fib2;
+  while (true) {
+    let tempfib1: number = fib1;
+    fib1 = fib2;
+    fib2 += tempfib1;
+    yield fib1 + fib2;
+  }
+}
 
-/**
- * const gen = fibGenerator();
- * gen.next().value; // 0
- * gen.next().value; // 1
+/* const gen = fibGenerator();
+gen.next().value; // 0
+gen.next().value; // 1
+gen.next().value; // 1
+gen.next().value; // 2
+gen.next().value; // 3
+gen.next().value; // 5
+gen.next().value; // 8
+gen.next().value; // 13
  */
