e3dae4cc26b3fa82bf6b54d50b09c0ee70dbf0b6
chadjcampbell <chadjcampbell@gmail.com>
2023-06-03 10:51:14 -0400

    nested-array-generator (done)

--- 2649.nested-array-generator.ts
+++ 2649.nested-array-generator.ts
@@ -2,7 +2,15 @@ type MultidimensionalArray = (MultidimensionalArray | number)[];
 
 function* inorderTraversal(
   arr: MultidimensionalArray
-): Generator<number, void, unknown> {}
+): Generator<number, void, unknown> {
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      yield* inorderTraversal(arr[i] as MultidimensionalArray);
+    } else {
+      yield arr[i] as number;
+    }
+  }
+}
 
 /**
  * const gen = inorderTraversal([1, [2, 3]]);
