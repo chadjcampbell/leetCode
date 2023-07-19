9388cbf7045a45166fe558d74e8d81d66e637400
ChadHomeLaptop <chadjcampbell@gmail.com>
2023-07-19 16:28:11 -0400

    first python proble,m setup!

---
+++ 435.non-overlapping-intervals.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=435 lang=python3
+#
+# [435] Non-overlapping Intervals
+#
+
+# @lc code=start
+from typing import List
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        return 
+# @lc code=end
