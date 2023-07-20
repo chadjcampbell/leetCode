44fa220d02c5a4d7e233be0bfd8783e1253360a9
chadjcampbell <chadjcampbell@gmail.com>
2023-07-20 18:21:15 -0400

    min-stack python done

--- 155.min-stack.py
+++ 155.min-stack.py
@@ -5,22 +5,28 @@
 #
 
 # @lc code=start
+
 class MinStack:
 
     def __init__(self):
-        
+        self.stack = []
+        self.min = 2**31
 
     def push(self, val: int) -> None:
-        
+        if val <= self.min or not self.stack:
+            self.min = val
+        self.stack.append({'val': val, 'min': self.min})
 
     def pop(self) -> None:
-        
+        self.stack.pop()
+        if self.stack:
+            self.min = self.stack[-1]['min']
 
     def top(self) -> int:
-        
+        return self.stack[-1]['val']
 
     def getMin(self) -> int:
-        
+        return self.min
 
 
 # Your MinStack object will be instantiated and called as such:
@@ -30,4 +36,3 @@ class MinStack:
 # param_3 = obj.top()
 # param_4 = obj.getMin()
 # @lc code=end
-
