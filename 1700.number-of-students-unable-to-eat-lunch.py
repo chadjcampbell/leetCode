ace34ce56ad5d4b6814f0a3c97cf07d4eb328810
chadjcampbell <chadjcampbell@gmail.com>
2023-07-22 16:20:36 -0400

    number-of-students WIP python

--- 1700.number-of-students-unable-to-eat-lunch.py
+++ 1700.number-of-students-unable-to-eat-lunch.py
@@ -6,9 +6,24 @@
 
 # @lc code=start
 from typing import List
+from collections import deque
 
 
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        return
+        count = 0
+        q = deque(students)
+        while q:
+            for i in range(len(q)):
+                if count == len(q):
+                    return count
+                if q[i] == sandwiches[i]:
+                    q.pop()
+                    q.pop()
+                    count = 0
+                    break
+                else:
+                    q.append(q.popleft())
+                    count += 1
+        return 0
 # @lc code=end
