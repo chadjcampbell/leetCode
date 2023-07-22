8e74f15204c0f6706f6e5f3ebd65b8b0bd721ed3
chadjcampbell <chadjcampbell@gmail.com>
2023-07-22 13:26:50 -0400

    design-browser-history python done

--- 1472.design-browser-history.py
+++ 1472.design-browser-history.py
@@ -5,19 +5,37 @@
 #
 
 # @lc code=start
+
+class BrowserNode:
+    def __init__(self, url: str) -> None:
+        self.prev = None
+        self.next = None
+        self.url = url
+
+
 class BrowserHistory:
 
     def __init__(self, homepage: str):
-        
+        self.head = BrowserNode(homepage)
+        self.curr = self.head
 
     def visit(self, url: str) -> None:
-        
+        node = BrowserNode(url)
+        self.curr.next = node
+        node.prev = self.curr
+        self.curr = node
 
     def back(self, steps: int) -> str:
-        
+        while self.curr.prev and steps:
+            self.curr = self.curr.prev
+            steps -= 1
+        return self.curr.url
 
     def forward(self, steps: int) -> str:
-        
+        while self.curr.next and steps:
+            self.curr = self.curr.next
+            steps -= 1
+        return self.curr.url
 
 
 # Your BrowserHistory object will be instantiated and called as such:
@@ -26,4 +44,3 @@ class BrowserHistory:
 # param_2 = obj.back(steps)
 # param_3 = obj.forward(steps)
 # @lc code=end
-
