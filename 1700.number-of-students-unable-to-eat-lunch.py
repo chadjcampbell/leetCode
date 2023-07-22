e68670e86831826d23d595a156c9cd0363d780ea
chadjcampbell <chadjcampbell@gmail.com>
2023-07-22 15:58:51 -0400

    number-of-students python start

---
+++ 1700.number-of-students-unable-to-eat-lunch.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=1700 lang=python3
+#
+# [1700] Number of Students Unable to Eat Lunch
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        return
+# @lc code=end
