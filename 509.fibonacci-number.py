a61e0c6b8130c9f81c36ac689e402de85a640d7e
ChadHomeLaptop <chadjcampbell@gmail.com>
2023-07-23 11:17:06 -0400

    fibonacci resursive works

--- 509.fibonacci-number.py
+++ 509.fibonacci-number.py
@@ -5,8 +5,16 @@
 #
 
 # @lc code=start
+# resursive
+""" class Solution:
+    def fib(self, n: int) -> int:
+        if n < 2:
+            return n
+        return self.fib(n-1) + self.fib(n-2)
+ """
+
+
 class Solution:
     def fib(self, n: int) -> int:
-        
+        return
 # @lc code=end
-
