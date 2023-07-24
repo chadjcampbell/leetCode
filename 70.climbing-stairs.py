864356645ad563caa329d6c86382337774860d31
chadjcampbell <chadjcampbell@gmail.com>
2023-07-24 18:08:09 -0400

    climbing-stairs recursive memo python

--- 70.climbing-stairs.py
+++ 70.climbing-stairs.py
@@ -6,11 +6,21 @@
 
 # @lc code=start
 # dp solution
-class Solution:
+""" class Solution:
     def climbStairs(self, n: int) -> int:
         a, b = 1, 1
         for _ in range(n):
             a, b = b, a+b
         return a
+ """
 
+# recursive with memoization solution
+
+
+class Solution:
+    def climbStairs(self, n: int, memo={1: 1, 2: 2, 3: 3}) -> int:
+        if n in memo:
+            return memo[n]
+        memo[n] = self.climbStairs(n-1)+self.climbStairs(n-2)
+        return memo[n]
 # @lc code=end
