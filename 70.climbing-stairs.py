b69db7b1731d6e16e01d3c2a02d6288581745fad
chadjcampbell <chadjcampbell@gmail.com>
2023-07-24 17:51:33 -0400

    climbing-stairs dp python

---
+++ 70.climbing-stairs.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=70 lang=python3
+#
+# [70] Climbing Stairs
+#
+
+# @lc code=start
+# dp solution
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a, b = 1, 1
+        for _ in range(n):
+            a, b = b, a+b
+        return a
+
+# @lc code=end
