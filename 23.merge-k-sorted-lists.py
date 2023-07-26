f5c5776f549c55d693aa0e14664df7c1fe3ac822
ChadHomeLaptop <chadjcampbell@gmail.com>
2023-07-26 08:59:40 -0400

    add imports and class

--- 23.merge-k-sorted-lists.py
+++ 23.merge-k-sorted-lists.py
@@ -6,12 +6,16 @@
 
 # @lc code=start
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+from typing import List, Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        
+        return
 # @lc code=end
-
