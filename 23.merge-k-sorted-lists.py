be83f4bfe4812a74907b62baac569651a5a197fc
ChadHomeLaptop <chadjcampbell@gmail.com>
2023-07-26 09:29:11 -0400

    merge k lists python WIP

--- 23.merge-k-sorted-lists.py
+++ 23.merge-k-sorted-lists.py
@@ -17,5 +17,28 @@ class ListNode:
 
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        return
+        finalHead = ListNode()
+        finalCurr = finalHead
+
+        def mergeTwo(L1, L2):
+            dummyHead = ListNode()
+            curr = dummyHead
+            while L1 and L2:
+                if (L1.val <= L2.val):
+                    curr.next = L1
+                else:
+                    curr.next = L2
+                curr = curr.next
+            while L1:
+                curr.next = L1
+                curr = curr.next
+            while L2:
+                curr.next = L2
+                curr = curr.next
+            return dummyHead.next
+
+        for i in range(len(lists)):
+            finalCurr.next = mergeTwo(lists[i-1], lists[i])
+        return finalHead.next
+
 # @lc code=end
