b2725d7cf3ce8ce7a98e20d925fca81d55345c74
chadjcampbell <chadjcampbell@gmail.com>
2023-07-28 10:58:25 -0400

    kth largest in array python WIP

--- 215.kth-largest-element-in-an-array.py
+++ 215.kth-largest-element-in-an-array.py
@@ -9,6 +9,15 @@ from typing import List
 
 
 class Solution:
+    def partition(self, nums, s, e):
+        key = nums[e]
+        j = s
+        for i in range(nums[s], nums[e]):
+            if nums[i] <= key:
+                nums[j], nums[i] = nums[i], nums[j]
+                j += 1
+        return
+
     def findKthLargest(self, nums: List[int], k: int) -> int:
         return
 # @lc code=end
