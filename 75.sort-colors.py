37500ce888aa311640300e9b89ec755f5a53ab2c
chadjcampbell <chadjcampbell@gmail.com>
2023-07-29 14:16:26 -0400

    sort colors python WIP

--- 75.sort-colors.py
+++ 75.sort-colors.py
@@ -10,8 +10,14 @@ from typing import List
 
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+        counts = [0, 0, 0]
+        for num in range(len(nums)):
+            counts[num] += 1
+
+        i = 0
+        for c in range(len(counts)):
+            for n in range(c):
+                nums[i] = n
+                i += 1
 
 # @lc code=end
