1db21bc971c69251d8c8942673db9aab54529946
chadjcampbell <chadjcampbell@gmail.com>
2023-08-01 12:07:23 -0400

    coin change WIP

--- 322.coin-change.py
+++ 322.coin-change.py
@@ -5,10 +5,26 @@
 #
 
 # @lc code=start
+import math
 from typing import List
 
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        return -1
+        memo = {0: 0}
+
+        def dfs(target):
+            if target in memo:
+                return memo[target]
+            if target < 0:
+                return math.inf
+            if target == 0:
+                memo[target] = target
+            for coin in coins:
+                memo[target] = min(math.inf, 1 + dfs(target-coin))
+            return memo[target]
+
+        return dfs(amount) if dfs(amount) != math.inf else -1
+
+
 # @lc code=end
