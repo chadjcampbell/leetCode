37171d96c516220f5b9cfb9af8e4d224b28e1eac
chadjcampbell <chadjcampbell@gmail.com>
2023-08-01 11:48:38 -0400

    coin change python start

---
+++ 322.coin-change.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=322 lang=python3
+#
+# [322] Coin Change
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        return -1
+# @lc code=end
