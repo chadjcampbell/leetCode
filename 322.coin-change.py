2958b88bb1463ba3b521910aade4e384e478c18c
chadjcampbell <chadjcampbell@gmail.com>
2023-08-01 14:36:32 -0400

    coin change python done

--- 322.coin-change.py
+++ 322.coin-change.py
@@ -14,17 +14,25 @@ class Solution:
         memo = {0: 0}
 
         def dfs(target):
-            if target in memo:
-                return memo[target]
             if target < 0:
                 return math.inf
             if target == 0:
-                memo[target] = target
+                return target
+            if target in memo:
+                return memo[target]
+
+            memo[target] = math.inf
+
             for coin in coins:
-                memo[target] = min(math.inf, 1 + dfs(target-coin))
+                memo[target] = min(memo[target], 1 + dfs(target-coin))
+
             return memo[target]
 
-        return dfs(amount) if dfs(amount) != math.inf else -1
+        result = dfs(amount)
+        if result != math.inf:
+            return result
+        else:
+            return -1
 
 
 # @lc code=end
