cc04ccf3c5ba2ee886945ff7b25b8a78b576fedc
chadjcampbell <chadjcampbell@gmail.com>
2024-03-29 10:33:20 -0400

    2958 clean up working solution

--- 2958.Length-of-Longest-Subarray-With-at-Most-K-Frequency.py
+++ 2958.Length-of-Longest-Subarray-With-at-Most-K-Frequency.py
@@ -8,24 +8,15 @@ class Solution:
         left = 0
         kmap = {}
         for right, n in enumerate(nums):
-            if n in kmap:
+            if n in kmap and kmap[n] != 0:
                 kmap[n] += 1
             else:
                 kmap[n] = 1
-            if kmap[n] <= k:
-                current_max = right - left + 1
-                if current_max > result:
-                    result = current_max
-                if kmap[n] > k:
-                    kmap[nums[left]] -= 1
-                    left += 1
+            while kmap[n] > k:
+                kmap[nums[left]] -= 1
+                left += 1
+            current_max = right - left + 1
+            if current_max > result:
+                result = current_max
+
         return result
-
-
-""" Currently 738/992 test cases passed needs fixed """
-
-""" Example
-Input: nums = [1,2,3,1,2,3,1,2], k = 2
-Output: 6
-Explanation: The longest possible good subarray is [1,2,3,1,2,3] since the values 1, 2, and 3 occur at most twice in this subarray. Note that the subarrays [2,3,1,2,3,1] and [3,1,2,3,1,2] are also good.
-It can be shown that there are no good subarrays with length more than 6. """
