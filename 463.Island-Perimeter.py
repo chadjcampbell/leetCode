099c636cd8ba6c7e0e4ea54357d74995f4cff6c3
chadjcampbell <chadjcampbell@gmail.com>
2024-04-18 14:32:56 -0400

    463 simplified conditionals

--- 463.Island-Perimeter.py
+++ 463.Island-Perimeter.py
@@ -6,13 +6,15 @@ class Solution:
         res = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
+                # if you hit a 1, check all 4 sides
                 if grid[i][j] == 1:
-                    if j - 1 < 0 or (j > 0 and grid[i][j-1] == 0):
+                    # on each side, check if it's out of bounds or 0
+                    if j - 1 < 0 or grid[i][j-1] == 0:
                         res += 1
-                    if i - 1 < 0 or (i > 0 and grid[i-1][j] == 0):
+                    if i - 1 < 0 or grid[i-1][j] == 0:
                         res += 1
-                    if j + 1 == len(grid[0]) or (j < len(grid[0]) - 1 and grid[i][j+1] == 0):
+                    if j + 1 == len(grid[0]) or grid[i][j+1] == 0:
                         res += 1
-                    if i + 1 == len(grid) or (i < len(grid) - 1 and grid[i+1][j] == 0):
+                    if i + 1 == len(grid) or grid[i+1][j] == 0:
                         res += 1
         return res
