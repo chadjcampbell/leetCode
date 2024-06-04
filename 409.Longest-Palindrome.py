4a9e45108b624ff873046675182d7ed3a255711f
chadjcampbell <chadjcampbell@gmail.com>
2024-06-04 18:48:24 -0400

    409 wip

---
+++ 409.Longest-Palindrome.py
@@ -0,0 +1,7 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        res = 0
+        sorted_s = "".join(sorted(s))
+        for i, c in enumerate(sorted_s):
+            # ...
+        return res
